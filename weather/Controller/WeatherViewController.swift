1280ccedef2ed959e89157b4a3b34b5fa7371e52
shardik95 <shardik95@ccs.neu.edu>
2018-08-07 03:33:45 -0400

    Initial storyboard with icons and constaints

---
+++ weather/Controller/WeatherViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by Hardik Shah on 8/7/18.
+//  Copyright © 2018 Hardik Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
