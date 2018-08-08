47457fec16b6d2954a24d1ae03b4b2bcc74cd58b
shardik95 <shardik95@ccs.neu.edu>
2018-08-08 01:13:15 -0400

    Tapped into the GPS

--- weather/Controller/WeatherViewController.swift
+++ weather/Controller/WeatherViewController.swift
@@ -7,16 +7,25 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let API_ID = ""
 
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var weatherIcon: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +33,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count-1]
+        if location.horizontalAccuracy > 0{
+            locationManager.stopUpdatingLocation()
+        }
+        let lat = String(location.coordinate.latitude)
+        let lon = String(location.coordinate.longitude)
+        
+        let params : [String:String] = ["lat":lat,"lon":lon,"appid":API_ID]
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        cityLabel.text = "Location Unavailable"
+        print(error)
+    }
 
 }
 
