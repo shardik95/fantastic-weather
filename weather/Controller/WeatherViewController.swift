fe48acfb8e2404cc7c43248ff4670aef10e9ae8b
shardik95 <shardik95@ccs.neu.edu>
2018-08-08 11:39:00 -0400

    Received data using api

--- weather/Controller/WeatherViewController.swift
+++ weather/Controller/WeatherViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 import CoreLocation
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let API_ID = ""
+    let API_ID = "b82fd2cc1179ec80efbc6636f685ae11"
 
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var weatherIcon: UIImageView!
@@ -32,6 +34,19 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func getWeatherData(url:String, parameters:[String:String]){
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON{
+            response in
+            if response.result.isSuccess {
+                let weatherData : JSON = JSON(response.result.value!)
+                self.updateWeatherData(json: weatherData);
+            }
+            else{
+                self.cityLabel.text = "Connection Issues"
+            }
+        }
+    }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations[locations.count-1]
@@ -43,11 +58,17 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         let params : [String:String] = ["lat":lat,"lon":lon,"appid":API_ID]
         
+        getWeatherData(url:WEATHER_URL,parameters: params)
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         cityLabel.text = "Location Unavailable"
         print(error)
+    }
+    
+    func updateWeatherData(json: JSON){
+        let tempResult = json["main"]["temp"]
+        print(tempResult)
     }
 
 }
